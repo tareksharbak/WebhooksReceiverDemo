384ebb21e2551c7f7cd03d44ce63ba551b4d754b
Tarek Sharbak <tarek@viloc.eu>
2018-01-25 12:12:37 +0100

    Initial Commit

---
+++ WebhooksReceiverTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebhooksReceiverTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebhooksReceiverTest/CustomWebhookHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.WebHooks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebhooksReceiverTest
+{
+	//STEP: 3-Create webhook handler derived from WebhookHandler
+	public class CustomWebhookHandler : WebHookHandler
+	{
+		public override async Task ExecuteAsync(string generator, WebHookHandlerContext context)
+		{
+			var content = await context.Request.Content.ReadAsStringAsync();
+			Trace.WriteLine(string.Format("Generator: {0} - Content: {1}", generator, content));
+		}
+	}
+}
